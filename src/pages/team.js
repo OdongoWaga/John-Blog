318215cc6c2927bf1e4e1ce5a6bcbddc1d0b6d5c
Waga Odongo <wagaodongo@gmail.com>
2019-03-29 11:31:46 +0300

    styling
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

---
+++ src/pages/team.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+import SEO from "../components/seo"
+
+const TeamPage = () => (
+  <Layout>
+    <SEO title="Team" keywords={[`gatsby`, `application`, `react`]} />
+    <h1> Team </h1>
+  </Layout>
+)
+
+export default TeamPage
