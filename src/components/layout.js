8ccf142f1bad334dd107e6ec2fff943ceb653e1e
Waga Odongo <wagaodongo@gmail.com>
2019-04-13 15:08:40 +0300

    added social media icons
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,18 +1,13 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
-
 import Header from "./header"
+import Footer from "./Footer"
+import Sidebar from "./Sidebar"
 import "../styles/index.scss"
+import { Row, Col } from "reactstrap"
 
-const Layout = ({ children }) => (
+const Layout = ({ authorImageFluid, children, pageTitle, postAuthor }) => (
   <StaticQuery
     query={graphql`
       query SiteTitleQuery {
@@ -25,15 +20,28 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
+        <link
+          rel="stylesheet"
+          href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
+          integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"
+          crossOrigin="anonymous"
+        />
+
         <Header siteTitle={data.site.siteMetadata.title} />
+
         <div className="container" id="content">
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
+          <h1>{pageTitle}</h1>
+
+          <Row>
+            <Col md="8">{children}</Col>
+
+            <Col md="4">
+              <Sidebar author={postAuthor} authorFluid={authorImageFluid} />
+            </Col>
+          </Row>
         </div>
+
+        <Footer />
       </>
     )}
   />
