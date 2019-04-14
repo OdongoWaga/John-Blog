f141447cee0fe86d15c0dce74088748d0287cd1c
Waga Odongo <wagaodongo@gmail.com>
2019-04-14 16:22:03 +0300

    shortname
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,16 +1,14 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
-  <Layout pageTitle="Oops">
+  <Layout pageTitle="Oups, Somethig went wrong..">
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    <Link classname="btn btn-primary text-uppercase" to={"/"}>
-      Go home{" "}
+
+    <Link className="btn btn-primary text-uppercase" to={"/"}>
+      Go home
     </Link>
   </Layout>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -35,7 +35,6 @@ const IndexPage = () => {
                   tags={node.frontmatter.tags}
                 />
               ))}
-
               <PaginationLinks currentPage={1} numberOfPages={numberOfPages} />
             </div>
           )
@@ -49,7 +48,6 @@ const indexQuery = graphql`
   query indexQuery {
     allMarkdownRemark(
       sort: { fields: [frontmatter___date], order: DESC }
-
       limit: 2
     ) {
       totalCount
@@ -57,16 +55,11 @@ const indexQuery = graphql`
       edges {
         node {
           id
-
           frontmatter {
             title
-
             date(formatString: "MMM Do YYYY")
-
             author
-
             tags
-
             image {
               childImageSharp {
                 fluid(maxWidth: 600) {
